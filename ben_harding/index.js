ad4cfd705e1f83b52da125a21381d5ebfb835285
bharding2 <b.harding@gmail.com>
2016-04-04 22:36:11 -0700

    combined index back into greet.js

--- ben_harding/index.js
+++
@@ -1,6 +0,0 @@
-const greet = require (__dirname + '/lib/greet.js');
-
-var name = 'Slothbear';
-
-if (process.argv[2]) name = process.argv[2];
-console.log(greet(name));
--- ben_harding/lib/greet.js
+++ ben_harding/lib/greet.js
@@ -1,3 +1,8 @@
 const greet = module.exports = function(name) {
   return 'Hello ' + name;
 };
+
+var name = 'Slothbear';
+
+if (process.argv[2]) name = process.argv[2];
+console.log(greet(name));
